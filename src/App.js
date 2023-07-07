ce8ffd34633e6c3fdbd1989d41d3c31bfe4183c8
Tecnologías de la Información <tic.calama@inacap.cl>
2023-07-07 14:55:32 -0400

    comentario

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {useEffect,useState} from "react";
+import ReactDom from "react-dom/client";
+import { Route, Routes, Link } from "react-router-dom";
+
+const App = () => {
+  return (
+      <>
+        <Routes>
+            <Route path="/" element={}/>
+            <Route path="/" element={}/>
+        </Routes>
+      </>
+  )
+}
+
+export default App;
