3738f681241f8272fd4bd5ae8879f9329a65b3f7
Mata-Zano <cupelt@hotmial.com>
2023-07-14 12:04:32 -0400

    Arreglo de formula

--- src/components/Ejercicio.jsx
+++ src/components/Ejercicio.jsx
@@ -1,37 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Ejercicio = () => {
   const [numero, setNumero] = useState(0);
+  const [factorial, setFactorial] = useState(0);
 
-  const handleValue = (evento) =>{
+  const handleValue = (evento) => {
     setNumero(evento.target.value);
-  }
-  const handleNumero = (evento) => {
-    console.log(numero);
-    var resultado;
-    for (let i = 1; i < numero.length; i++) {
-      resultado = numero * i;
-    }
-    return alert("Su numero es :" + resultado);
   };
+
+  const calcularFactorial = () => {
+    let resultado = 1;
+    for (let i = 1; i <= numero; i++) {
+      resultado *= i;
+    }
+    setFactorial(resultado);
+  };
+
   return (
     <>
       <h1>Bienvenido a Ejercicio</h1>
       <label htmlFor="numero">Numero :</label>
       <br />
-      <input 
-      type="number" 
-      placeholder="Ingrese un numero"
-      onChange={handleValue} />
+      <input
+        type="number"
+        placeholder="Ingrese un numero"
+        onChange={handleValue}
+      />
       <br />
       <br />
-      <button type="button" onClick={handleNumero}>
+      <button type="button" onClick={calcularFactorial}>
         Calcular Factorial
       </button>
       <br />
       <br />
-
+      <p>
+        El factorial de {numero} es: {factorial}
+      </p>
+      <br />
       <Link to={"/"}>Ir a Inicio</Link>
       <br />
     </>
